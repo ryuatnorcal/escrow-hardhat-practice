b4d425b55206cb06629a65d86a5ec646c5558c59
ryuatnorcal <rmatsuda686@gmail.com>
2024-07-11 10:29:45 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+import deploy from './deploy';
+import Escrow from './Escrow';
+import axios from 'axios';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+export async function approve(escrowContract, signer) {
+  const approveTxn = await escrowContract.connect(signer).approve();
+  await approveTxn.wait();
+}
+
+function App() {
+  const [escrows, setEscrows] = useState([]);
+  const [account, setAccount] = useState();
+  const [signer, setSigner] = useState();
+
+  useEffect(() => {
+    async function getAccounts() {
+      const accounts = await provider.send('eth_requestAccounts', []);
+
+      setAccount(accounts[0]);
+      setSigner(provider.getSigner());
+    }
+
+    getAccounts();
+  }, [account]);
+
+  async function newContract() {
+    const wei = ethers.utils.parseEther(document.getElementById('ether').value)
+    const beneficiary = document.getElementById('beneficiary').value;
+    const arbiter = document.getElementById('arbiter').value;
+    const value = ethers.BigNumber.from(wei);
+    const escrowContract = await deploy(signer, arbiter, beneficiary, value);
+
+
+    const escrow = {
+      address: escrowContract.address,
+      arbiter,
+      beneficiary,
+      value: value.toString(),
+      handleApprove: async () => {
+        escrowContract.on('Approved', () => {
+          document.getElementById(escrowContract.address).className =
+            'complete';
+          document.getElementById(escrowContract.address).innerText =
+            "✓ It's been approved!";
+        });
+
+        await approve(escrowContract, signer);
+      },
+    };
+
+    setEscrows([...escrows, escrow]);
+    // add something to keep track on deployed escrow in server side
+    sendRecord(escrow);
+  }
+
+  async function sendRecord(escrow){
+    await axios(process.env.RECORD_URL, {
+      method: 'POST',
+      data: escrow
+    })
+  }
+
+  return (
+    <>
+      <div className="contract">
+        <h1> New Contract </h1>
+        <label>
+          Arbiter Address
+          <input type="text" id="arbiter" />
+        </label>
+
+        <label>
+          Beneficiary Address
+          <input type="text" id="beneficiary" />
+        </label>
+
+        <label>
+          Deposit Amount (Ether)
+          <input type="text" id="ether" />
+        </label>
+
+        <div
+          className="button"
+          id="deploy"
+          onClick={(e) => {
+            e.preventDefault();
+
+            newContract();
+          }}
+        >
+          Deploy
+        </div>
+      </div>
+
+      <div className="existing-contracts">
+        <h1> Existing Contracts </h1>
+
+        <div id="container">
+          {escrows.map((escrow) => {
+            return <Escrow key={escrow.address} {...escrow} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
